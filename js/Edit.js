1e42fb868ab80a8a3907f4701e39e3fb377b00b6
DawidWebb <tomasz.matras@developerweb.pl>
2020-07-04 09:02:48 +0200

    add fn to ste max & min Sec, Min, Hrs

--- js/Edit.js
+++ js/Edit.js
@@ -6,20 +6,34 @@ class Edit extends Timer {
   }
   editTimer = () => {
     this.editActive = !this.editActive;
-    console.log("edit" + " " + this.editActive);
     this.edit.classList.toggle("btnActive");
     this.divHrs.classList.toggle("timerActive");
     this.divMin.classList.toggle("timerActive");
     this.divSec.classList.toggle("timerActive");
+    if (this.divHrs.value < 0) {
+      this.divHrs.value = 0;
+    } else if (this.divHrs.value > 24) {
+      this.divHrs.value = 24;
+    }
+    if (this.divMin.value < 0) {
+      this.divMin.value = 0;
+    } else if (this.divMin.value > 60) {
+      this.divMin.value = 60;
+    }
+    if (this.divSec.value < 0) {
+      this.divSec.value = 0;
+    } else if (this.divSec.value > 60) {
+      this.divSec.value = 60;
+    }
 
     this.editTime();
   };
   editTime = () => {
-    if (this.edit.className === "function-timing__edit btnActive") {
+    if (this.editActive === true) {
       this.divHrs.removeAttribute("disabled");
       this.divMin.removeAttribute("disabled");
       this.divSec.removeAttribute("disabled");
-    } else if (this.edit.className === "function-timing__edit") {
+    } else if (this.editActive === false) {
       this.divHrs.setAttribute("disabled", true);
       this.divMin.setAttribute("disabled", true);
       this.divSec.setAttribute("disabled", true);
